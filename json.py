f26f4524980fd1179f83a49d3e3d44b4cd976cb7
marktouch <marcellatouch@comcast.net>
2023-11-08 10:33:19 -0800

    testing

---
+++ json.py
@@ -0,0 +1,12 @@
+# imports json class library
+import json
+
+# creates the data dictionary
+data = {
+    'name': 'John Doe',
+    'age': 30,
+    'city': 'New York, NY',
+    'interests': ['golf','running','videogames'],
+    'is_student':False
+}
+# testing
