f6ca649fa0e209d75509c4ef44017e43b48ee664
marktouch <marcellatouch@comcast.net>
2023-11-20 03:01:38 -0800

    JSON Python assignment

--- main.py
+++ main.py
@@ -15,3 +15,7 @@ with open('data.json', 'w') as json_file:
     json.dump(data,json_file,indent=4)
 
 print("data has been written to data.json")
+
+# 
+with open('data.json','r') as json_file:
+    loaded_data = json.load(json_file)
